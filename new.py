d9f85661bee1af419e3849618dcd1138cd84a06a
kanishka prabasara <51412148+Kanishka00@users.noreply.github.com>
2021-10-11 22:40:22 +0530

    Update new.py

--- new.py
+++ new.py
@@ -3,4 +3,5 @@ print("This is a test python file")
 print("this is also another python line")
 print("this is new")
 "this is only on new2"
-this is a test
+pprriinntt((this is a test
+           ))
